e5501cc83bd8670acbf35ecc7f5c0ffad5ad3fb4
Vladimir Khanyakin <vladimir.khanyakin@mytona.com>
2020-07-24 19:56:42 +0300

    Color updaters

---
+++ Visual/ColorChangers/ImageColorChanger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Common.Visual
+{
+    [RequireComponent(typeof(Image))]
+    public class ImageColorChanger : ColorChangerBase
+    {
+        private Image _img;
+
+        private void Awake()
+        {
+            _img = GetComponent<Image>();
+        }
+
+        public override void SetColor(Color color)
+        {
+            _img.color = color;
+        }
+
+        public override Color GetColor()
+        {
+            return _img.color;
+        }
+    }
+}
+
