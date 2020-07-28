d1d8341d805b3e7384dee6ad844e8b48fdb69c08
Vladimir Khanyakin <vladimir.khanyakin@mytona.com>
2020-07-28 15:15:54 +0300

    Common helping functions

---
+++ Static/CommonHelpingFunctions.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Common.StaticHelpers
+{
+    public static class CommonHelpingFunctions
+    {
+        public static Vector3 RandomPointInBox(Vector3 center, Vector3 size)
+        {
+            return center + new Vector3(
+                       (Random.value - 0.5f) * size.x,
+                       (Random.value - 0.5f) * size.y,
+                       (Random.value - 0.5f) * size.z
+                   );
+        }
+    }
+}
