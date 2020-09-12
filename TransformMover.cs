92aa3df2623a6f249d03a9eb2703c4e520eab19b
intfloatbool <intfloatbool@gmail.com>
2020-09-12 14:16:48 +0300

    transform mover

---
+++ TransformMover.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Common 
+{
+    public class TransformMover : MonoBehaviour
+    {
+        [SerializeField] private bool _isEnabled = true;
+        public bool IsEnabled
+        {
+            get { return _isEnabled; }
+            set { this._isEnabled = value; }
+        }
+        
+        [SerializeField] private Vector3 _direction = Vector3.forward;
+        public Vector3 Direction
+        {
+            get { return _direction; }
+            set { this._direction = value; }
+        }
+        
+        [SerializeField] private float _speed = 5f;
+        public float Speed
+        {
+            get { return _speed; }
+            set { this._speed = value; }
+        }
+        
+        void Update()
+        {
+            transform.Translate(_direction * _speed * Time.deltaTime);
+        }
+    } 
+}
