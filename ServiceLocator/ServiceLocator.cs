a1a88d415c90b09cf48992bc30f13848ac4a55b2
intfloatbool <intfloatbool@gmail.com>
2020-12-01 13:00:27 +0300

    Service locator base

---
+++ ServiceLocator/ServiceLocator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Common.ServiceLocator
+{
+    public class ServiceLocator
+    {
+        private ServiceLocator() {  }
+        
+        private readonly Dictionary<string, IGameService> _services = new Dictionary<string, IGameService>();
+        
+        public static ServiceLocator Current { get; private set; }
+        
+        public static void Initialize()
+        {
+            Current = new ServiceLocator();
+        }
+
+        public T Get<T>() where T : IGameService
+        {
+            string key = typeof(T).Name;
+
+            return default(T);
+
+        }
+    }
+}
+
