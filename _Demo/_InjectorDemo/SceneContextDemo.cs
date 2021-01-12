e6da9ec9064ff5446810ec27c562981917dd1b97
intfloatbool <intfloatbool@gmail.com>
2021-01-12 11:15:12 +0300

    Injector demo

---
+++ _Demo/_InjectorDemo/SceneContextDemo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Injection;
+using UnityEngine;
+
+namespace UnityCommonHelpers._Demo._InjectorDemo
+{
+    /// <summary>
+    /// This class is responsibility for setup any dependency for each 
+    /// </summary>
+    public class SceneContextDemo : MonoBehaviour
+    {
+        [SerializeField] private CubeRotatorData _cubeRotatorData;
+        [SerializeField] private CubeRotator _cubeRotator;
+        
+        private Injector _injector;
+        private void Awake()
+        {
+            _injector = new Injector();
+            _injector.Bind(new Calculator());
+            _injector.Bind(_cubeRotatorData);
+            
+            
+            _injector.Bind(_cubeRotator);
+            
+            _injector.PostBindings();
+            //_injector.Bind();
+        }
+    }
+}
