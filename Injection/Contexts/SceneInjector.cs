9b2ee19d2e431d355b6b4cea456951ac9a5435a0
intfloatbool <intfloatbool@gmail.com>
2021-02-11 16:01:01 +0500

    Scene and global context + MonoInjectors

---
+++ Injection/Contexts/SceneInjector.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using Injection;
+using System.Linq;
+
+namespace UnityCommonHelpers.Injection.Contexts
+{
+    public class SceneInjector : MonoBehaviour {
+        [SerializeField] protected MonoInjectorBase[] _monoInjectors;
+        protected Injector _sceneInjector;
+
+        void OnValidate()
+        {
+            if(_monoInjectors != null && !_monoInjectors.Any()) {
+                _monoInjectors = FindObjectsOfType<MonoInjectorBase>();
+            }
+        }
+
+        protected virtual void Awake() {
+            var globalInjector = GlobalInjector.Instance;
+
+            _sceneInjector = globalInjector == null ? _sceneInjector = new Injector() 
+                : _sceneInjector = new Injector(globalInjector);
+
+
+            InitScene();
+        }
+
+        protected virtual void InitScene() {
+            BindMonoInjectors();
+        }
+
+        protected void BindMonoInjectors() {
+            foreach(var monoInj in _monoInjectors) {
+                monoInj.Init(_sceneInjector);
+            }
+        }
+
+    }
+}
